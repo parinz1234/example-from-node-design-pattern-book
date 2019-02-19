e45e6493219b3376db3fc8d8e3efcee47238fec9
parinz1234 <parinya.ons@gmail.com>
2019-02-19 22:59:28 +0700

    hardcode dependency

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const authController = require('./lib/authController'); 
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello world !'))
+app.post('/login', authController.login); 
+app.get('/checkToken', authController.checkToken); 
 
 app.listen(3000 , () => {
   console.log(`Server running on port 3000`)
