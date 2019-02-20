68e6780e97e597cecbe54bbdbb005a2bf3bff928
parinz1234 <parinya.ons@gmail.com>
2019-02-20 23:28:01 +0700

    .

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const authController = require('./lib/authController'); 
+
+const authControllerFactory = require('./lib/authController'); 
+const authServiceFactory = require('./lib/authService');
+const dbFactory = require('./lib/db');
 
 const app = express()
 app.use(express.json())
+
+const db = dbFactory('example-db')
+const authService = authServiceFactory(db, 'SHHH!')
+const authController = authControllerFactory(authService)
 
 app.post('/login', authController.login); 
 app.get('/checkToken', authController.checkToken); 
